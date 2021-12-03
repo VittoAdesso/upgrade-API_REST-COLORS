2fad8f839d412a48b73bbdde1e79571c2e103606
Vittorio Adesso <adessovittorio8@gmail.com>
2021-12-03 18:11:54 +0100

    Migrado a index.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Node Server listen in PORT: ${PORT}`)
-});
+// move to index
