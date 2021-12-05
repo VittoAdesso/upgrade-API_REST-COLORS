24e6c34d78919086f754b6a5b26e4d30471b3b3f
Vittorio Adesso <adessovittorio8@gmail.com>
2021-12-05 19:38:02 +0100

    Fase de pruebas

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 
 const { connect } = require("./app/config/database");
 
-const users = require("./app/api/routes/user.routes");
+// const users = require("./app/api/routes/user.routes");
 const colors = require("./app/api/routes/color.routes");
-const palettes = require("./app/api/routes/palette.routes");
+// const palettes = require("./app/api/routes/palette.routes");
 
 const HTTPSTATUSCODE = require("./app/utils/httpStatusCode");
 const cors = require("cors");
@@ -14,6 +14,8 @@ const PORT = 3001;
 connect();
 
 const app = express();
+
+//headers 
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -37,8 +39,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
 app.use("/colors", colors);
-// app.use("/palettes", palettes); 
-// app.use("/users", users)
+app.use("/palettes", palettes); 
+app.use("/users", users)
 
 
 app.use((req, res, next) => {
@@ -50,6 +52,8 @@ app.use((req, res, next) => {
 
 
 app.set("secretKey", "nodeRestApi");
+app.set("secretKey", "nodeRestApi"); // jwt secret token
+
 
 //Manejo  errores
 app.use((err, req, res, next) => {
@@ -61,5 +65,5 @@ app.disable("x-powered-by");
 
 //Levantamos el servidor
 app.listen(PORT, () => {
-    console.log(`Node is listening in: ${PORT}`);
+    console.log(`Node is listenning in: ${PORT}`);
 });
